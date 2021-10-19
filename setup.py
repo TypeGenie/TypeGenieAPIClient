636ba9d34d46d469d3ff1e61dd177d4adac027f9
typegeniedeveloper <codebuild@aws.com>
2021-10-19 09:55:48 +0000

    Codebuild bumpversion

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ src_dir = str(Path(__file__).parent)
 
 setup(
     name='typegenie',
-    version='0.3.0',
+    version='0.3.1',
     url="https://github.com/TypeGenie/TypeGenieApiClient",
     author="abhitopia",
     author_email="hi@typegenie.net",
